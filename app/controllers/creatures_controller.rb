d88ba988c029f74ecbf4c064afa5780c1e103704
michaelscotthurley <michaelscotthurley>
2016-03-03 17:39:10 -0800

    Minimum project requirements

---
+++ app/controllers/creatures_controller.rb
@@ -0,0 +1,37 @@
+class CreaturesController < ApplicationController
+  def index
+    @creatures = Creature.all
+  end
+
+  def create
+    Creature.create creature_params
+    redirect_to creatures_path
+  end
+
+  def new
+    @creature = Creature.new
+  end
+
+  def edit
+    @creature = Creature.find(params[:id])
+  end
+
+  def show
+    @creature = Creature.find(params[:id])
+  end
+
+  def update
+    Creature.find(params[:id]).update creature_params
+    redirect_to creatures_path
+  end
+
+  def destroy
+    Creature.find(params[:id]).destroy
+    redirect_to creatures_path
+  end
+
+  private
+  def creature_params
+      params.require(:creature).permit(:name, :description)
+  end
+end
